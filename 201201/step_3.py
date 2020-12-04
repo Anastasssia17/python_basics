276f3d74509573dd79325b080699099b29f154e4
Nastya <manturova.10@mail.ru>
2020-12-05 01:01:46 +0500

    step 3

---
+++ 201201/step_3.py
@@ -0,0 +1,23 @@
+pupil_1 = 'Настя'
+pupil_2 = 'Женя'
+pupil_3 = 'Ира'
+
+print('группа 28:', pupil_1, pupil_2, pupil_3)
+
+# VS
+
+group_28 = ['Настя', 'Женя', 'Ира']
+#              0        1      2
+print('группа 28:', group_28)
+print(group_28[0], group_28[2])
+print(type(group_28), dir(group_28))
+
+group_28.reverse()
+print(group_28)
+group_28.sort()
+print(group_28)
+
+
+
+
+
